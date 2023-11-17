223789f25a6263f553677202006e60fc0da63b69
qkiet1002 <qkiet2k4@gmail.com>
2023-11-17 19:24:49 +0700

    17/11 giao diện tôi

--- app/src/main/java/com/example/duan1_nhom4/fragment/ToiFragment.java
+++ app/src/main/java/com/example/duan1_nhom4/fragment/ToiFragment.java
@@ -1,5 +1,6 @@
 package com.example.duan1_nhom4.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,79 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.duan1_nhom4.Login.LoginApp;
 import com.example.duan1_nhom4.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ToiFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ToiFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    View btnToiVocher;
+    View btnToiDiaChi;
+    View btnToiDMK;
+    View btnToiDaThich;
+    View btnToiGioHang;
+    View btnToiLogOut;
 
     public ToiFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ToiFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ToiFragment newInstance(String param1, String param2) {
-        ToiFragment fragment = new ToiFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_toi, container, false);
+        View view = inflater.inflate(R.layout.fragment_toi, container, false);
+        btnToiVocher = view.findViewById(R.id.btnToiVocher);
+        btnToiDiaChi = view.findViewById(R.id.btnToiDiaChi);
+        btnToiDaThich = view.findViewById(R.id.btnToiDaThich);
+        btnToiDMK = view.findViewById(R.id.btnToiDMK);
+        btnToiGioHang = view.findViewById(R.id.btnToiGioHang);
+        btnToiLogOut = view.findViewById(R.id.btnToiLogOut);
+
+        btnToiVocher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Vocher của bạn", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnToiDiaChi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Địa chỉ", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnToiDaThich.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Đã thích", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnToiDMK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Đổi mật khẩu", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnToiGioHang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Giỏ hàng", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnToiLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Đăng xuất", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getContext(), LoginApp.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
